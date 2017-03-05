cdbfca3e6162b877327adf9a0c0546f029bf633e
Laxmikant Ratnaparkhi <lkratnaparkhi@gmail.com>
2017-03-05 15:42:06 +0530

    First Commit

---
+++ insti/core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, template_name="index.html")
+
+def sidebar(request):
+    return render(request, template_name="listing-row-sidebar.html")
